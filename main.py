3dafe06bf59d6c61fefea7aa771a70328a406a8d
aarifulbdtech-web <aariful.bdtech@gmail.com>
2026-09-03 00:59:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from repl-nix-workspace!")
+import os
+from flask import Flask # or from fastapi import FastAPI
 
+app = Flask(__name__)
+
+# Fetch database URL from Render Environment Variables
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+@app.route("/")
+def home():
+    return "App is live on Render!"
 
 if __name__ == "__main__":
-    main()
+    # Render assigns dynamic port numbers via $PORT
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
